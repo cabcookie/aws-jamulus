aadaad21bb09df2c6fdb10779471922162421729
Carsten Koch <carsten.b.koch@gmail.com>
2021-05-16 22:46:20 +0200

    feat: updated images for jamulus and zoom server

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -10,12 +10,12 @@ new DigitalWorkstation(app, 'DigitalWorkstation', {
   bandServerSettings: {
     ipId: 'eipalloc-4d0de976',
     settingsFileName: 'band-server-settings.sh',
-    // imageId: 'ami-',
+    imageId: 'ami-0ed221cd21a330912',
   },
   mixingResultServerSettings: {
     ipId: 'eipalloc-7680094d',
     settingsFileName: 'mixing-server-settings.sh',
-    // imageId: 'ami-',
+    imageId: 'ami-0afd1c88bcb335aba',
   },
   onlineMixerSettings: {
     ipId: 'eipalloc-3baa7e00',
@@ -23,6 +23,7 @@ new DigitalWorkstation(app, 'DigitalWorkstation', {
   },
   zoomServerSettings: {
     ipId: 'eipalloc-f1eba0ca',
+    imageId: 'ami-0b51ce9c72dc98095',
     zoomMeeting: {
       meetingId: '123456789012',
     },
--- utilities/policies/ssm-permissions.ts
+++ utilities/policies/ssm-permissions.ts
@@ -1,5 +1,10 @@
 import { Effect, PolicyStatement } from "@aws-cdk/aws-iam";
 
+/**
+ * Creates an IAM policy statement to give Session Manager access to the resource.
+ * 
+ * @returns A PolicyStatement allowing Session Manager to access resource
+ */
 export const createSsmPermissions = (): PolicyStatement => new PolicyStatement({
   actions: [
     "ssmmessages:*",
