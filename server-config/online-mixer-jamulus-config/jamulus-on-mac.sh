53bcd55716fca4825276cd515d84ab859c8edf82
Carsten Koch <carsten.b.koch@gmail.com>
2021-05-20 17:56:20 +0200

    feat: just some jamulus client config testing

---
+++ server-config/online-mixer-jamulus-config/jamulus-on-mac.sh
@@ -0,0 +1,9 @@
+/Applications/Jamulus.app/Contents/MacOS/Jamulus -c 3.66.182.113 --clientname Acoustic -i jamulus-on-mac/Acoustic.ini &
+/Applications/Jamulus.app/Contents/MacOS/Jamulus -c 3.66.182.113 --clientname Bass -i jamulus-on-mac/Bass.ini &
+/Applications/Jamulus.app/Contents/MacOS/Jamulus -c 3.66.182.113 --clientname Choir -i jamulus-on-mac/Choir.ini &
+/Applications/Jamulus.app/Contents/MacOS/Jamulus -c 3.66.182.113 --clientname Drums -i jamulus-on-mac/Drums.ini &
+/Applications/Jamulus.app/Contents/MacOS/Jamulus -c 3.66.182.113 --clientname Electric2 -i jamulus-on-mac/Electric2.ini &
+/Applications/Jamulus.app/Contents/MacOS/Jamulus -c 3.66.182.113 --clientname Keys1 -i jamulus-on-mac/Keys1.ini &
+/Applications/Jamulus.app/Contents/MacOS/Jamulus -c 3.66.182.113 --clientname Keys4 -i jamulus-on-mac/Keys4.ini &
+/Applications/Jamulus.app/Contents/MacOS/Jamulus -c 3.66.182.113 --clientname Loop2 -i jamulus-on-mac/Loop2.ini &
+/Applications/Jamulus.app/Contents/MacOS/Jamulus -c 3.66.182.113 --clientname Ruth -i jamulus-on-mac/Ruth.ini
