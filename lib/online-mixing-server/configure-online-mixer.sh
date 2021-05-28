b9ffd8de612511c5e8a5be47f2553b8dc951d26a
Carsten Koch <carsten.b.koch@gmail.com>
2021-05-29 01:51:05 +0200

    feat: apps are fully installed
    During EC2 instance initialization of the
    Online Mixer instance we create a startup
    script `install-apps.sh` which will run when
    a user first logs in to Ubuntu Desktop. This
    is ensured by the `install-apps.desktop`file
    which will be deleted after the app
    installations, so that it will run only once.
    We also set real-time process permissions
    for the user `ubuntu`.
    And we ensure the Ubuntu instance is using
    a German keyboard layout.

--- lib/online-mixing-server/configure-online-mixer.sh
+++ lib/online-mixing-server/configure-online-mixer.sh
@@ -8,15 +8,38 @@ echo y | sudo apt-get install awscli
 
 LOG install basic Ubuntu Desktop
 echo y | sudo apt-get install --no-install-recommends ubuntu-desktop
+sudo apt-get -y install `check-language-support -l de`
+
+LOG install gnome startup applications
+sudo apt-get install gnome-startup-applications
 
 LOG install ubuntu remote desktop environment
 echo y | sudo apt-get install xrdp
 sudo service gdm3 start
 sudo systemctl enable xrdp
 
+LOG set real-time process permissions for `ubuntu` user
+sudo sed -i 's/# End of file/@audio          -       rtprio          99\n@audio          -       memlock         unlimited\n\n# End of file/' /etc/security/limits.conf
+echo "# Provided by the jackd package." >> audio.conf
+echo "#" >> audio.conf
+echo "# Changes to this file will be preserved." >> audio.conf
+echo "#" >> audio.conf
+echo "# If you want to enable/disable realtime permissions, run" >> audio.conf
+echo "#" >> audio.conf
+echo "#    dpkg-reconfigure -p high jackd" >> audio.conf
+echo " " >> audio.conf
+echo "@audio   -  rtprio     95" >> audio.conf
+echo "@audio   -  memlock    unlimited" >> audio.conf
+echo "#@audio   -  nice      -19" >> audio.conf
+sudo mv audio.conf /etc/security/limits.d/
+sudo addgroup audio
+sudo usermod -a -G audio ubuntu
+
 LOG copy configuration files to local Documents folder
 mkdir /home/ubuntu/Documents
-aws s3 cp s3://jamulus-config-bucket/online-mixer-jamulus-config/ /home/ubuntu/Documents/ --recursive --exclude "*" --include "jamulus*"
+sudo aws s3 cp s3://jamulus-config-bucket/online-mixer-jamulus-config/ /home/ubuntu/Documents/ --recursive --exclude "*" --include "jamulus*"
+sudo aws s3 cp s3://jamulus-config-bucket/ardour/ /home/ubuntu/Documents/mosaik-live/ --recursive --include "*"
+chmod +x /home/ubuntu/Documents/jamulus-startup.sh
 
 LOG install the CloudWatch agent
 # see download links for different OS here: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/download-cloudwatch-agent-commandline.html
@@ -33,12 +56,25 @@ sudo mv config.json /opt/aws/amazon-cloudwatch-agent/bin/
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
 
 LOG prepare startup file to install remaining apps – Ardour and Jamulus
-echo "echo y | sudo apt-get install ardour" >> /home/ubuntu/Documents/install-apps.sh
-echo "sudo wget https://github.com/jamulussoftware/jamulus/releases/download/r3_7_0/jamulus_3.7.0_ubuntu_amd64.deb" >> /home/ubuntu/Documents/install-apps.sh
-echo "sudo apt install ./jamulus_3.7.0_ubuntu_amd64.deb" >> /home/ubuntu/Documents/install-apps.sh
-echo "sudo rm ./jamulus_3.7.0_ubuntu_amd64.deb" >> /home/ubuntu/Documents/install-apps.sh
-chmod 755 /home/ubuntu/Documents/install-apps.sh
-cat /home/ubuntu/Documents/install-apps.sh
+mkdir /home/ubuntu/bin
+echo "setxkbmap -layout de" >> /home/ubuntu/bin/install-apps.sh
+echo "sudo apt-get -y install ardour" >> /home/ubuntu/bin/install-apps.sh
+echo "sudo wget https://github.com/jamulussoftware/jamulus/releases/download/r3_7_0/jamulus_3.7.0_ubuntu_amd64.deb" >> /home/ubuntu/bin/install-apps.sh
+echo "sudo apt install ./jamulus_3.7.0_ubuntu_amd64.deb" >> /home/ubuntu/bin/install-apps.sh
+echo "sudo rm ./jamulus_3.7.0_ubuntu_amd64.deb" >> /home/ubuntu/bin/install-apps.sh
+echo "sudo rm /etc/xdg/autostart/install-apps.desktop"  >> /home/ubuntu/bin/install-apps.sh
+chmod +x /home/ubuntu/bin/install-apps.sh
+
+sudo chown -R ubuntu /home/ubuntu/bin/
+sudo chown -R ubuntu /home/ubuntu/Documents
+echo "[Desktop Entry]" >> install-apps.desktop
+echo "Type=Application" >> install-apps.desktop
+echo "Name=Audio-Anwendungen-Installation" >> install-apps.desktop
+echo "Exec=/home/ubuntu/bin/install-apps.sh" >> install-apps.desktop
+echo "Terminal=true" >> install-apps.desktop
+sudo mv install-apps.desktop /etc/xdg/autostart/
+cat /etc/xdg/autostart/install-apps.desktop
+cat home/ubuntu/bin/install-apps.sh
 
 LOG create a password for user ubuntu
 echo -e "%%UBUNTU_PASSWORD%%\n%%UBUNTU_PASSWORD%%" | sudo passwd ubuntu
