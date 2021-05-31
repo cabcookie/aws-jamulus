99c182bb63d71aea7d555115ebd702c31b3b2519
Carsten Koch <carsten.b.koch@gmail.com>
2021-05-31 15:17:00 +0200

    fix: change startup order for Windows
    Start Voicemeter before Jamulus and Zoom

--- server-config/zoom-server-config/Jamulus_Startup.bat
+++ server-config/zoom-server-config/Jamulus_Startup.bat
@@ -1,4 +1,5 @@
+start C:\"Program Files (x86)"\VB\Voicemeeter\voicemeeterpro.exe -L"C:\Users\Administrator\Desktop\Voicemeeter-Config\JamulusToZoomServer.xml"
+
 start C:\"Program Files"\Jamulus\Jamulus.exe --clientname ToZoom -i jamulus-inis/JamulusToZoom.ini -c %JAMULUS_MIXER_IP% -M
 start C:\"Program Files"\Jamulus\Jamulus.exe --clientname FromZoom -i jamulus-inis/JamulusFromZoom.ini -c %JAMULUS_BAND_IP%
 start C:\"Program Files (x86)"\Zoom\bin\Zoom.exe --url=https://us02web.zoom.us/j/%MEETING_ID%%MEETING_PASSWORD%
-start C:\"Program Files (x86)"\VB\Voicemeeter\voicemeeterpro.exe -L"C:\Users\Administrator\Desktop\Voicemeeter-Config\JamulusToZoomServer.xml"
