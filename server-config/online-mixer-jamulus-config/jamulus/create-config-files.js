7fc33a87205813d7939dd65783fb810dbf12b7dd
Carsten Koch <carsten.b.koch@gmail.com>
2021-05-31 11:18:29 +0200

    fix: missed the `create-config-files.js`
    The file is launched by the `configure-online-mixer.sh` to create the
    `jamulus-startup.sh` and the corresponding ini files.

---
+++ server-config/online-mixer-jamulus-config/jamulus/create-config-files.js
@@ -0,0 +1,58 @@
+const { readFileSync, writeFileSync } = require("fs");
+
+const folder = process.argv[2];
+const bandIp = process.argv[3];
+const defIni = readFileSync(`${folder}/default.ini`, 'utf-8');
+const stdFader = readFileSync(`${folder}/storedfader.ini`, 'utf-8');
+
+const createEncodedName = (clientName) => Buffer.from(clientName, 'utf-8').toString('base64');
+
+const pushAChannel = (channelName, isMuted, settings, fader) => settings.push(fader.replace(/%%CLIENT_NAME%%/, createEncodedName(channelName)).replace(/%%IS_MUTE%%/, isMuted ? 1 : 0));
+
+const pushAnEmptyChannel = (channels, index, settings, fader) => index >= channels.length * 2
+  ? settings.push(fader.replace(/%%CLIENT_NAME%%/, '').replace(/%%IS_MUTE%%/, 0))
+  : null;
+
+const createMixerChannelName = (channel) => `Mixer for ${channel}`;
+
+const pushAMixerChannel = (channels, index, settings, fader) => index >= channels.length && index < channels.length * 2
+  ? pushAChannel(createMixerChannelName(channels[index]), true, settings, fader)
+  : null;
+
+const pushAPeerChannel = (channels, index, channel, settings, fader) => index < channels.length
+  ? pushAChannel(channels[index], channels[index] !== channel, settings, fader)
+  : null;
+
+const { channels } = JSON.parse(readFileSync(`${folder}/channels.json`, 'utf-8'));
+const startJamulusLine = `jamulus -c ${bandIp} --clientname "%%CLIENT_NAME%%" -i "jamulus-inis/%%CLIENT_NAME%%.ini" -M &`;
+const jamulusStartup = [];
+
+channels.forEach(channel => {
+  // for each channel we will create two ini-files
+
+  // one ini-file for the server
+  const faderSettings = [];
+  const serverIniFile = defIni.replace(/%%CLIENT_NAME%%/g, createEncodedName(createMixerChannelName(channel)))
+  for (let index = 0; index < 250; index++) {
+    let fader = stdFader.replace(/%%ID%%/g, index);
+    pushAnEmptyChannel(channels, index, faderSettings, fader);
+    pushAMixerChannel(channels, index, faderSettings, fader);
+    pushAPeerChannel(channels, index, channel, faderSettings, fader);
+  }
+  writeFileSync(`${folder}jamulus-inis/mixer ${channel}.ini`, serverIniFile.replace(/%%STORED_FADERS%%/, faderSettings.join('\n')));
+  jamulusStartup.push(startJamulusLine.replace(/%%CLIENT_NAME%%/g, createMixerChannelName(channel)));
+
+  // one ini-file for the user
+
+});
+jamulusStartup.push('echo "Starting all Jamulus instances..."');
+writeFileSync(`${folder}jamulus-startup.sh`, jamulusStartup.join('\n'));
+
+// console.log(JSON.stringify(channels));
+// console.log(stdFader);
+
+
+
+// const clientName = "Carsten";
+// const encoded = Buffer.from(clientName, 'utf-8').toString('base64');
+// console.log(`${clientName}: ${encoded}`);
