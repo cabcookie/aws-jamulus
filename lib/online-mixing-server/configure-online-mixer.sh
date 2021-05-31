70c640ecd4ea6761ba380160a47187bf3dcdba70
Carsten Koch <carsten.b.koch@gmail.com>
2021-05-31 16:41:16 +0200

    fix: log permissions and content of files

--- lib/online-mixing-server/configure-online-mixer.sh
+++ lib/online-mixing-server/configure-online-mixer.sh
@@ -1,6 +1,11 @@
 LOG() {
     command echo $(date) "[USER DATA]" "$@"
 }
+SHOW_FILE() {
+    command echo $(date) "[USER DATA] SHOW FILE" "$@"
+    command ls -l $@
+    command cat $@
+}
 
 LOG update packages
 sudo apt-get update
@@ -32,12 +37,16 @@ echo "@audio   -  rtprio     95" >> audio.conf
 echo "@audio   -  memlock    unlimited" >> audio.conf
 echo "#@audio   -  nice      -19" >> audio.conf
 sudo mv audio.conf /etc/security/limits.d/
-sudo addgroup audio
 sudo usermod -a -G audio ubuntu
+
+SHOW_FILE /etc/security/limits.d/audio.conf
 
 LOG fetch Jack configuration files
 sudo aws s3 cp s3://jamulus-config-bucket/online-mixer-jamulus-config/jack/jackdrc.conf /home/ubuntu/.jackdrc
 sudo aws s3 cp s3://jamulus-config-bucket/online-mixer-jamulus-config/jack/conf.xml /home/ubuntu/.config/jack/
+
+SHOW_FILE /home/ubuntu/.jackdrc
+SHOW_FILE /home/ubuntu/.config/jack/conf.xml
 
 LOG install node
 echo y | sudo apt-get install nodejs
@@ -57,9 +66,7 @@ node $CFGFOLD/create-config-files.js $CFGFOLD %%BAND_PRIVATE_IP%% %%BAND_PUBLIC_
 sudo mv $CFGFOLD/jamulus* /home/ubuntu/Documents/
 chmod +x /home/ubuntu/Documents/jamulus-startup.sh
 
-LOG show ini files and jamulus-startup.sh
-ls /home/ubuntu/Documents/jamulus-inis
-cat /home/ubuntu/Documents/jamulus-startup.sh
+SHOW_FILE /home/ubuntu/Documents/jamulus-startup.sh
 
 LOG add Ardour project to Documents
 sudo aws s3 cp s3://jamulus-config-bucket/ardour/ /home/ubuntu/Documents/mosaik-live/ --recursive --include "*"
@@ -68,7 +75,9 @@ sudo chown -R ubuntu /home/ubuntu/Documents
 LOG create wrapper app Audio Workstation
 sudo aws s3 cp s3://jamulus-config-bucket/online-mixer-jamulus-config/app-wrapper/jamulus-startup.desktop /usr/share/applications/
 sudo aws s3 cp s3://jamulus-config-bucket/online-mixer-jamulus-config/app-wrapper/jamulus-startup.png /usr/share/icons/
-chmod a+x /usr/share/applications/jamulus-startup.desktop
+# chmod a+x /usr/share/applications/jamulus-startup.desktop
+
+SHOW_FILE /usr/share/applications/jamulus-startup.desktop
 
 LOG install the CloudWatch agent
 # see download links for different OS here: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/download-cloudwatch-agent-commandline.html
@@ -76,21 +85,24 @@ wget https://s3.%%REGION%%.amazonaws.com/amazoncloudwatch-agent-%%REGION%%/ubunt
 echo yes | sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 rm amazon-cloudwatch-agent.deb
 
-LOG load and show the config file
+LOG load the CloudWatch config file
 aws s3 cp s3://jamulus-config-bucket/cloudwatch-linux-settings.json config.json
-cat config.json
 
 LOG move the config file and start the CloudWatch agent
 sudo mv config.json /opt/aws/amazon-cloudwatch-agent/bin/
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+
+SHOW_FILE /opt/aws/amazon-cloudwatch-agent/bin/config.json
 
 LOG prepare startup file to install remaining apps – Ardour and Jamulus
 sudo aws s3 cp s3://jamulus-config-bucket/online-mixer-jamulus-config/app-wrapper/install-apps.sh /home/ubuntu/bin/
 sudo aws s3 cp s3://jamulus-config-bucket/online-mixer-jamulus-config/app-wrapper/install-apps.desktop /etc/xdg/autostart/
 chmod +x /home/ubuntu/bin/install-apps.sh
 sudo chown -R ubuntu /home/ubuntu/bin/
-cat /etc/xdg/autostart/install-apps.desktop
-cat /home/ubuntu/bin/install-apps.sh
+
+SHOW_FILE /etc/xdg/autostart/install-apps.desktop
+SHOW_FILE /home/ubuntu/bin/install-apps.sh
+SHOW_FILE /usr/share/applications/jamulus-startup.desktop
 
 LOG create a password for user ubuntu
 echo -e "%%UBUNTU_PASSWORD%%\n%%UBUNTU_PASSWORD%%" | sudo passwd ubuntu
