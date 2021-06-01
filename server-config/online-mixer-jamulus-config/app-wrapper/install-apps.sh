c462ce554f12f5e1707aaef31effd48155c56439
Carsten Koch <carsten.b.koch@gmail.com>
2021-06-01 08:57:45 +0200

    fix: ensuring the language pack is permanent
    The language pack was restored after every restart. So, I put the
    command to set the language into a shell script and started that
    instead. Also, I changed the startup script to set the language after
    every reboot.

--- server-config/online-mixer-jamulus-config/app-wrapper/install-apps.sh
+++ server-config/online-mixer-jamulus-config/app-wrapper/install-apps.sh
@@ -1,8 +1,11 @@
-setxkbmap -layout de
+echo "setxkbmap -layout de" > set-language-pack-de.sh
+chmod +x set-language-pack-de.sh
+./set-language-pack-de.sh
 sudo apt-get -y install ardour
 sudo wget https://github.com/jamulussoftware/jamulus/releases/download/r3_7_0/jamulus_3.7.0_ubuntu_amd64.deb
 sudo apt install ./jamulus_3.7.0_ubuntu_amd64.deb
 sudo rm ./jamulus_3.7.0_ubuntu_amd64.deb
-sudo rm /etc/xdg/autostart/install-apps.desktop
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'jamulus-startup.desktop']"
+sudo sed -i 's/install-apps.sh/set-language-pack-de.sh/' /etc/xdg/autostart/install-apps.desktop
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'jamulus-startup.desktop']"
 # gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'jamulus-startup.desktop']"
+# sudo reboot
