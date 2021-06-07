d4d2cde0adfa34aecbfd065282b67c41b4031d2c
Carsten Koch <carsten.b.koch@gmail.com>
2021-06-07 11:36:21 +0200

    fix(infra): switch off animations on Ubuntu

--- server-config/AudioWorkstation/app-wrapper/install-apps.sh
+++ server-config/AudioWorkstation/app-wrapper/install-apps.sh
@@ -2,11 +2,8 @@ echo "setxkbmap -layout de" > bin/autostart-workstation.sh
 chmod +x bin/autostart-workstation.sh
 ./bin/autostart-workstation.sh
 gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'jamulus-startup.desktop']"
-# gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'jamulus-startup.desktop']"
+gsettings set org.gnome.desktop.interface enable-animations false
 echo "./Documents/jamulus/jamulus-startup.sh" >> bin/autostart-workstation.sh
-
-# DEBFILE=jamulus_3.7.0_ubuntu_amd64.deb
-# sudo wget https://github.com/jamulussoftware/jamulus/releases/download/r3_7_0/$DEBFILE
 
 DEBFILE=jamulus_3.8.0_ubuntu_amd64.deb
 sudo wget https://github.com/jamulussoftware/jamulus/releases/download/r3_8_0/$DEBFILE
